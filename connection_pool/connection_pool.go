2dc69e55c3718bef237ca8b4ca09e9d801f8d3b4
vishal-k3gster <vishal.k3gster@gmail.com>
2017-02-28 13:25:56 +0000

    removed print and logs from connection_pool.

--- connection_pool/connection_pool.go
+++ connection_pool/connection_pool.go
@@ -2,25 +2,45 @@ package main
 
 import (
 "pool"
+"fmt"
 "os"
 )
 
 func main() {
 	//the pool size is of FOUR CONNECTIONS
 	os.Remove("./myDb.db")
-	pool.Init("./myDb.db")
-	
+	if (pool.Init("./myDb.db") == 1){
+		fmt.Println("pool is initialize")
+	}else{
+		fmt.Println("Initialization failed;")
+		return
+	}
+
 	check1 := pool.Get_Conn()
+	if(check1 == nil){
+		fmt.Println("Cant get connection from pool");
+	}else{
+		fmt.Println("connection successful");
+	}
 	check2 := pool.Get_Conn()
+	if(check2 == nil){
+		fmt.Println("Cant get connection from pool");
+	}else{
+		fmt.Println("connection successful");
+	}
 	check3 := pool.Get_Conn()
+	if(check3 == nil){
+		fmt.Println("Cant get connection from pool");
+	}else{
+		fmt.Println("connection successful");
+	}
+	// pool.Release_Conn(check1)
+	// pool.Get_Conn()
+	// pool.Get_Conn()
+	// pool.Get_Conn()
 
-	pool.Release_Conn(check1)
-	pool.Get_Conn()
-	pool.Get_Conn()
-	pool.Get_Conn()
-
-	pool.Release_Conn(check3)
-	pool.Release_Conn(check2)
-	pool.Get_Conn()
-	pool.Get_Conn()
+	// pool.Release_Conn(check3)
+	// pool.Release_Conn(check2)
+	// pool.Get_Conn()
+	// pool.Get_Conn()
 }
--- connection_pool/pool/pool.go
+++ connection_pool/pool/pool.go
@@ -2,12 +2,8 @@ package pool
 
 import (
 "database/sql"
-"fmt"
 _"github.com/mattn/go-sqlite3"
-"logger"
 "sync"
-"os"
-"strconv"
 )
 var db_path string
 var con_size,pull,push,req int
@@ -15,37 +11,23 @@ var connection_channel = make([]*sql.DB, 4)
 var busy_connection = make([]int ,4)
 var mu sync.Mutex
 
-func Init(path string) {
-	os.Remove("pool_log.text")
-	logger.Init("pool_log.text")
+func Init(path string) int {
 	db_path = path
 	con_size = 4
 	push,req = -1,-1
-	if (Insert_conn(con_size) == 1){
-		fmt.Println("SUCCESS")
-		logger.Info("generated the connection")
-	}else{
-		fmt.Println("FAILED")
-		logger.Error("generation of pool is failed")
-	}
+	return Insert_conn(con_size)
 }
 
 func Insert_conn(num int) int {
-	fmt.Println(con_size)	
 	for i:=0 ; i< num ; i++{
 		conn := make_connection(i)
 		if(conn != nil){
 			connection_channel[i] = conn
-			fmt.Println("connection "+strconv.Itoa(i)+" done successfully")
-			logger.Info("connection "+strconv.Itoa(i)+" done successfully")
 
 		}else{
-			fmt.Println("connection "+strconv.Itoa(i)+" Failed")
-			logger.Error("connection "+strconv.Itoa(i)+" Failed")
 			return 0
 		}
 	}
-	fmt.Println("database pool array = ", connection_channel)
 	return 1
 }
 
@@ -73,8 +55,6 @@ func Release_Conn(db *sql.DB) {
 	position := push%con_size
 	busy_connection[position] = 0
 	connection_channel[position] = db
-	fmt.Println("connection "+strconv.Itoa(position)+" release")
-	logger.Info("connection "+strconv.Itoa(position)+" release")
 }
 
 func Get_Conn() *sql.DB {
@@ -82,12 +62,8 @@ func Get_Conn() *sql.DB {
 	pull = req%con_size
 	if(check_available(pull)){
 		busy_connection[pull] = 1
-		fmt.Println("connection accuire")
-		 logger.Info("connection "+strconv.Itoa(pull)+" accuire successfully")
 		return connection_channel[pull]
 	}else{
-		fmt.Println("Pool is full")
-		logger.Warning("Pool is fool")
 		return nil
 	}
 
